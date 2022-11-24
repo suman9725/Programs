0637dacb5686acad6648d3d34d8cc7200970de23
Sounak <sounakofficial448@gmail.com>
2022-11-24 11:10:12 +0530

    Sorted Done! :)

--- SwapSort.java
+++ SwapSort.java
@@ -17,7 +17,7 @@ class SwapSort
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String: ");
         wrd = sc.nextLine();
-        wrd.toUpperCase();
+        wrd=wrd.toUpperCase();
     }
     void swapChar()
     {
@@ -39,9 +39,30 @@ class SwapSort
         a += ar[k];
         swapwrd = a;        
     }
+    void sortWord()
+    {
+        int temp;
+        char charArray[] = wrd.toCharArray();
+        len = wrd.length();
+        for (int i = 0; i < len; i++){
+            for(int j = i+1;j<len;j++)
+            {
+                if(charArray[i]>charArray[j]) 
+                {
+                    temp = charArray[i];
+                    charArray[i] = charArray[j];
+                    charArray[j] = (char) temp;
+                }
+            }   
+        }
+        for (int k = 0; k < len; k++) {
+            System.out.print(charArray[k]+"");
+        }
+
+    }
     void display()
     {
-        System.out.println("\nOriginal Word: "+wrd);
+        System.out.println("Original Word: "+wrd);
         System.out.println("Length of word: "+wrd.length());
         System.out.println("Swap Characters: "+ swapwrd);
     }
@@ -51,5 +72,6 @@ class SwapSort
         ob.read();
         ob.swapChar();
         ob.display();
+        ob.sortWord();
     }
 }
